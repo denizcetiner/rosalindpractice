894f61d06099269decf6627851d7110e9db97c2c
denizcetiner <deniz.cetiner94@gmail.com>
2018-06-09 17:48:33 +0300

    Added ID:RNA problem solution.

---
+++ RNA.py
@@ -0,0 +1,9 @@
+def run(dna_sequence = ""):
+    dna_sequence_highlighted = dna_sequence.replace("T", " T ")
+    rna_sequence_highlighted = dna_sequence.replace("T", " U ")
+    rna_sequence = dna_sequence.replace("T", "U")
+
+    print(dna_sequence_highlighted)
+    print(rna_sequence_highlighted)
+
+    return rna_sequence
