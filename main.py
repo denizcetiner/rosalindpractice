5448d7bbe05d60d00be37058f0f646978fe3381f
denizcetiner <deniz.cetiner94@gmail.com>
2018-06-09 17:38:55 +0300

    Added main method to run chosen problems by ID. Added ID:DNA problem solution which is same as ID:INI. URL and ID names are read from url_dataset_names.txt file.

---
+++ main.py
@@ -0,0 +1,21 @@
+def run(problem_name = ""):
+    url = values["{0}_url".format(problem_name)]
+    id = values["{0}_id".format(problem_name)]
+    print(url)
+    dataset_filename = "rosalind_{0}".format(id)
+    with open("{0}.txt".format(dataset_filename), "r") as dataset_file:
+        dataset_input = dataset_file.read().strip()
+    module = __import__(problem_name)
+    result = getattr(module, "run")(dataset_input)
+    with open("{0}_result.txt".format(dataset_filename), "w") as dataset_result_file:
+        dataset_result_file.write(result)
+    return
+
+values = {}
+with open("url_dataset_names.txt") as file:
+    for line in file:
+        (key, val) = line.split()
+        values[str(key)] = val
+
+problem_name = input()
+run(problem_name.upper())
