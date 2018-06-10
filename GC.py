1bd9eb351dcf4302e888a4d75e58dcdccad0814a
denizcetiner <deniz.cetiner94@gmail.com>
2018-06-10 13:00:03 +0300

    Added ID:GC problem solution.

---
+++ GC.py
@@ -0,0 +1,33 @@
+def extract(dna_strings=""):
+    dna_dict = {}
+    current_dna_name = ""
+    for line in dna_strings.splitlines():
+        if line.startswith(">"):
+            current_dna_name = line.replace(">", "")
+            dna_dict[current_dna_name] = ""
+        else:
+            dna_dict[current_dna_name] += line.strip()
+    return dna_dict
+
+
+def run(dna_strings=""">Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT"""):
+    dna_dict = extract(dna_strings)
+    dna_gc_dict = {}
+    for dna_name, dna_string in dna_dict.items():
+        g_count = dna_string.count("G")
+        c_count = dna_string.count("C")
+        ratio = (g_count + c_count) * 100 / len(dna_string)
+        dna_gc_dict[dna_name] = float("{0:.6f}".format(ratio))
+    print(dna_gc_dict)
+    max_gc_dna = max(dna_gc_dict, key=dna_gc_dict.get)
+    result = ("{0}\n{1}".format(max_gc_dna, dna_gc_dict[max_gc_dna]))
+    print(result)
+    return result
